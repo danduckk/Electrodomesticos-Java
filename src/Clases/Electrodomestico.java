42cff6e0f23dd22147aabe06797b3a8a25b1b353
danduck <daniselos3@gmail.com>
2026-02-18 12:29:04 +0100

    base de Electrodomestico

--- src/Clases/Electrodomestico.java
+++ src/Clases/Electrodomestico.java
@@ -1,4 +1,46 @@
 package Clases;
 
 public class Electrodomestico {
+    private String tipo;
+    private String marca;
+    private double potencia;
+
+    // CONSTRUCTOR
+    public Electrodomestico(String tipo, String marca, double potencia) {
+        this.tipo = tipo;
+        this.marca = marca;
+        this.potencia = potencia;
+    }
+
+    // SETTERS Y GETTERS
+    public String getTipo() {
+        return tipo;
+    }
+    public String getMarca() {
+        return marca;
+    }
+    public double getPotencia() {
+        return potencia;
+    }
+    public double getConsumo(int horas) {
+        return horas * potencia;
+    }
+    public double getCosteConsumo(int horas, double costeHora) {
+        return horas * costeHora;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    public void setPotencia(double potencia) {
+        this.potencia = potencia;
+    }
+
+    @Override
+    public String toString() {
+        return "Tipo: " + tipo + ", Marca: " + marca + ", Potencia: " + potencia;
+    }
 }
